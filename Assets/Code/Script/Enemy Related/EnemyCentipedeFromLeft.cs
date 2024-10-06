3d9f73d97d2997d7dffa7d4a8ea51bbdcf15b877
BlakeChiampou <blakeisalways@gmail.com>
2024-10-06 13:21:08 -0400

    Added Spiral and CentipedeL prefabs

--- Assets/Code/Script/Enemy Related/EnemyCentipedeFromLeft.cs
+++ Assets/Code/Script/Enemy Related/EnemyCentipedeFromLeft.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyCentipede : MonoBehaviour
+public class EnemyCentipedeFromLeft : MonoBehaviour
 {
     public float speed = 3;
-    public float negative = 1;
+    public float negative = -1;
     public bool flip = false;
+    public bool startMove = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,22 @@ public class EnemyCentipede : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(Vector3.left * speedDown * Time.deltaTime);
-        transform.Translate(Vector3.right * negative * speed * Time.deltaTime);
-        if(transform.position.x > 4.5 && flip == false) { //upper right corner (already at upper left when spawning)
-            transform.Rotate(0.0f, 0.0f, 270f, Space.Self); //rotate CW
-            flip = true; //do it once (would happen twice since changing to vertical movement doesnt affect horz position)
-        } 
-        if (transform.position.y < -3 && flip == true) { //bottom right
-            transform.Rotate(0.0f, 0.0f, 270f, Space.Self); //again
-            flip = false;
+        if (startMove){
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
-        if (transform.position.y < -3 && transform.position.x < -1) { //goes past bottom mid, comes back up
-            transform.Rotate(0.0f, 0.0f, 270f, Space.Self); //again
+
+        if(transform.position.x > 4.5 && flip == false) { //upper right corner (already at upper left when spawning)
+            transform.Translate(Vector3.down * speed * Time.deltaTime); //Rotate CW
+            //do it once (would happen twice since changing to vertical movement doesnt affect horz position)
+            startMove = false;
+        } 
+        if (transform.position.y < -3 && flip == false) { //bottom right
+            speed = speed * -1;
+            flip = true;
+        }
+        if (flip) {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
         }
     }
 }
