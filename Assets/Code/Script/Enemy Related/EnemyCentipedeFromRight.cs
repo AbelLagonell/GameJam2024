9638808efbe15ca2fd6f36d6fff8c6d64f84794d
BlakeChiampou <blakeisalways@gmail.com>
2024-10-06 13:51:51 -0400

    Added CentipedeR prefab

--- Assets/Code/Script/Enemy Related/EnemyCentipedeFromRight.cs
+++ Assets/Code/Script/Enemy Related/EnemyCentipedeFromRight.cs
@@ -7,6 +7,7 @@ public class EnemyCentipedeFromRight : MonoBehaviour
     public float speed = 3;
     public float negative = -1;
     public bool flip = false;
+    public bool startMove = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,22 @@ public class EnemyCentipedeFromRight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(Vector3.left * speedDown * Time.deltaTime);
-        transform.Translate(Vector3.right * negative * speed * Time.deltaTime);
-        if(transform.position.x < -4.5 && flip == false) { //upper Left corner (already at upper right when spawning)
-            transform.Rotate(0.0f, 0.0f, 90f, Space.Self); //rotate CCW
-            flip = true; //do it once (would happen twice since changing to vertical movement doesnt affect horz position)
-        } 
-        if (transform.position.y < -3 && flip == true) { //bottom left
-            transform.Rotate(0.0f, 0.0f, 90f, Space.Self); //again
-            flip = false;
+        if (startMove){
+            transform.Translate(Vector3.forward * negative * speed * Time.deltaTime);
         }
-        if (transform.position.y < -3 && transform.position.x > 1) { //goes past bottom mid, comes back up
-            transform.Rotate(0.0f, 0.0f, 90f, Space.Self); //again
+
+        if(transform.position.x < -4.5 && flip == false) { //upper right corner (already at upper left when spawning)
+            transform.Translate(Vector3.down * speed * Time.deltaTime); //Rotate CW
+            //do it once (would happen twice since changing to vertical movement doesnt affect horz position)
+            startMove = false;
+        } 
+        if (transform.position.y < -3 && flip == false) { //bottom right
+            speed = speed * -1;
+            flip = true;
+        }
+        if (flip) {
+            transform.Translate(Vector3.forward * negative * speed * Time.deltaTime);
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
         }
     }
 }
